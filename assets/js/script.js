7239982e5a90eabe108a1015eb924df97ca4f54d
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-04 14:22:59 -0400

    styling html

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+/*
+                                    Acceptance Criteria
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+*/
+
+/* Pseudo Code 
+
+html
+
+- Prompt with a bottom to start the quiz   
+- section for buttoms that will allow to change from one question to the other max 5 questions 
+- div for time interval top right and high scores top left with unique ID 
+- Each division, section, or boddy part of the html needs to have a unique ID to be able to access in the DOM
+- Display if it is a correct/wrong answer and then dissapear using time interval 
+- Main to carry all the quiz content inside 
+- header needs an id 
+
+css
+
+- Generic fonts, sizes, 
+- colors for buttom  
+- no backgrounds 
+- use flex for mobile response 
+
+js 
+
+- create variables for questions
+*/
