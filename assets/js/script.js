b50f6f3d085e155f0c804324bdcb89355a5d96e5
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-04 19:58:18 -0400

    array of questions and front page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,60 @@
-/*
-                                    Acceptance Criteria
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
-*/
+var headerVar = document.getElementById("header");
+var countDownVar = document.getElementById("countdown")
+console.log(countDownVar);
 
-/* Pseudo Code 
+var questions = [
+    {
+        question: "What's the short cut to create a default page in HTML?",
+        options: ["html:5", "HTML", "It does not exist", "html'5"],
+        answer: 0
+    },
+    {
+        question: "What's the first line in a HTML document?",
+        options: ["<docs html>","<!DOCTIPE html>", "<!DOCTYPE html>", "<html lang=>"],
+        answer: 2
+    },
+    {
+        question: "Which option is not true?",
+        options: ["var number = x;", "let number = y;","var arrNumber = [];", "div function() {};"],
+        answer: 3
+    },
+    {
+        question: "What's the attribute that indicates the destination of the hyperlink after the anchor <a ...?",
+        options: ["ref", "href","img","id"],
+        answer: 1
+    },
+    {
+        question: "How can you print a variable or a message in the debugging console in JavaScript?",
+        options: ["console.ra();", "console.log();", "console.val();", "console.911();"],
+        answer: 1
+    },
+    {
+        question: "What's the correct way to display an image in css?",
+        options: ["<img...", "background-image:url('...')","padding: ...", "display-img"],
+        answer: 2
+    },
+    {
+        question: "What DOM stands for in JavaScript?",
+        options: ["Document Object Model", "Document Object Modify","Document Other Model", "Days On Market"],
+        answer: 0
+    },
+    {
+        question: "How can you display the color inside this code :root {--primary-color=#fce138}?",
+        options: ["var(primary-color);","var(--primary-color);","var(-primary-color);", "primary-color();"],
+        answer: 1
+    },
+    {
+        question: "If you are in UCF BootCamp and you need help, what would be the first thing to do?",
+        options: ["call 911","ask in BCS Learning Assistant","watch movies to destress myself", "wait until class"],
+        answer: 1
+    },
+    {
+        question: "What's the default value for flex-direction?",
+        options: ["column", "center","space-between", "row"],
+        answer: 3
+    },
 
-html
+];
 
-- Prompt with a bottom to start the quiz   
-- section for buttoms that will allow to change from one question to the other max 5 questions 
-- div for time interval top right and high scores top left with unique ID 
-- Each division, section, or boddy part of the html needs to have a unique ID to be able to access in the DOM
-- Display if it is a correct/wrong answer and then dissapear using time interval 
-- Main to carry all the quiz content inside 
-- header needs an id 
-
-css
-
-- Generic fonts, sizes, 
-- colors for buttom  
-- no backgrounds 
-- use flex for mobile response 
-
-js 
-
-- create variables for questions
-*/
+console.log(questions);
+// array of 10 displayed
